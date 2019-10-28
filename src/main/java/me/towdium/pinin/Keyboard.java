8c22861b9b9c5a990ec5c8f6e2396c2c461cdb83
Juntong Liu <towdium@gmail.com>
2019-10-28 13:01:18 +0800

    Better formatting

--- src/main/java/me/towdium/pinin/Keyboard.java
+++ src/main/java/me/towdium/pinin/Keyboard.java
@@ -1,187 +1,68 @@
 package me.towdium.pinin;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Keyboard {
+    private static Map<String, String> DAQIAN_KEYS = Stream.of(new String[][]{
+            {"", ""}, {"0", ""}, {"1", " "}, {"2", "6"}, {"3", "3"},
+            {"4", "4"}, {"a", "8"}, {"ai", "9"}, {"an", "0"}, {"ang", ";"},
+            {"ao", "l"}, {"b", "1"}, {"c", "h"}, {"ch", "t"}, {"d", "2"},
+            {"e", "k"}, {"ei", "o"}, {"en", "p"}, {"eng", "/"}, {"er", "-"},
+            {"f", "z"}, {"g", "e"}, {"h", "c"}, {"i", "u"}, {"ia", "u8"},
+            {"ian", "u0"}, {"iang", "u;"}, {"iao", "ul"}, {"ie", "u,"}, {"in", "up"},
+            {"ing", "u/"}, {"iong", "m/"}, {"iu", "u."}, {"j", "r"}, {"k", "d"},
+            {"l", "x"}, {"m", "a"}, {"n", "s"}, {"o", "i"}, {"ong", "j/"},
+            {"ou", "."}, {"p", "q"}, {"q", "f"}, {"r", "b"}, {"s", "n"},
+            {"sh", "g"}, {"t", "w"}, {"u", "j"}, {"ua", "j8"}, {"uai", "j9"},
+            {"uan", "j0"}, {"uang", "j;"}, {"uen", "mp"}, {"ueng", "j/"}, {"ui", "jo"},
+            {"un", "jp"}, {"uo", "ji"}, {"v", "m"}, {"van", "m0"}, {"vang", "m;"},
+            {"ve", "m,"}, {"vn", "mp"}, {"w", "j"}, {"x", "v"}, {"y", "u"},
+            {"z", "y"}, {"zh", "5"},
+    }).collect(Collectors.toMap(d -> d[0], d -> d[1]));
+
+    private static Map<String, String> XIAOHE_KEYS = Stream.of(new String[][]{
+            {"ai", "d"}, {"an", "j"}, {"ang", "h"}, {"ao", "c"}, {"ch", "i"},
+            {"ei", "w"}, {"en", "f"}, {"eng", "g"}, {"ia", "x"}, {"ian", "m"},
+            {"iang", "l"}, {"iao", "n"}, {"ie", "p"}, {"in", "b"}, {"ing", "k"},
+            {"iong", "s"}, {"iu", "q"}, {"ong", "s"}, {"ou", "z"}, {"sh", "u"},
+            {"ua", "x"}, {"uai", "k"}, {"uan", "r"}, {"uang", "l"}, {"ui", "v"},
+            {"un", "y"}, {"uo", "o"}, {"ve", "t"}, {"ue", "t"}, {"vn", "y"},
+            {"zh", "v"},
+    }).collect(Collectors.toMap(d -> d[0], d -> d[1]));
+
+    private static Map<String, String> ZIRANMA_KEYS = Stream.of(new String[][]{
+            {"ai", "l"}, {"an", "j"}, {"ang", "h"}, {"ao", "k"}, {"ch", "i"},
+            {"ei", "z"}, {"en", "f"}, {"eng", "g"}, {"ia", "w"}, {"ian", "m"},
+            {"iang", "d"}, {"iao", "c"}, {"ie", "x"}, {"in", "n"}, {"ing", "y"},
+            {"iong", "s"}, {"iu", "q"}, {"ong", "s"}, {"ou", "b"}, {"sh", "u"},
+            {"ua", "w"}, {"uai", "y"}, {"uan", "r"}, {"uang", "d"}, {"ui", "v"},
+            {"un", "p"}, {"uo", "o"}, {"ve", "t"}, {"ue", "t"}, {"vn", "p"},
+            {"zh", "v"},
+    }).collect(Collectors.toMap(d -> d[0], d -> d[1]));
+
+    private static Map<String, String> PHONETIC_LOCAL = Stream.of(new String[][]{
+            {"yi", "i"}, {"you", "iu"}, {"yin", "in"}, {"ye", "ie"}, {"ying", "ing"},
+            {"wu", "u"}, {"wen", "un"}, {"yu", "v"}, {"yue", "ve"}, {"yuan", "van"},
+            {"yun", "vn"}, {"ju", "jv"}, {"jue", "jve"}, {"juan", "jvan"}, {"jun", "jvn"},
+            {"qu", "qv"}, {"que", "qve"}, {"quan", "qvan"}, {"qun", "qvn"}, {"xu", "xv"},
+            {"xue", "xve"}, {"xuan", "xvan"}, {"xun", "xvn"}, {"shi", "sh"}, {"si", "s"},
+            {"chi", "ch"}, {"ci", "c"}, {"zhi", "zh"}, {"zi", "z"}, {"ri", "r"},
+    }).collect(Collectors.toMap(d -> d[0], d -> d[1]));
+
     public static Keyboard QUANPIN = new Keyboard(null, null, Keyboard::standard, false);
-    private static HashMap<String, String> DAQIAN_KEYS = new HashMap<String, String>() {{
-        put("", "");
-        put("0", "");
-        put("1", " ");
-        put("2", "6");
-        put("3", "3");
-        put("4", "4");
-        put("a", "8");
-        put("ai", "9");
-        put("an", "0");
-        put("ang", ";");
-        put("ao", "l");
-        put("b", "1");
-        put("c", "h");
-        put("ch", "t");
-        put("d", "2");
-        put("e", "k");
-        put("ei", "o");
-        put("en", "p");
-        put("eng", "/");
-        put("er", "-");
-        put("f", "z");
-        put("g", "e");
-        put("h", "c");
-        put("i", "u");
-        put("ia", "u8");
-        put("ian", "u0");
-        put("iang", "u;");
-        put("iao", "ul");
-        put("ie", "u,");
-        put("in", "up");
-        put("ing", "u/");
-        put("iong", "m/");
-        put("iu", "u.");
-        put("j", "r");
-        put("k", "d");
-        put("l", "x");
-        put("m", "a");
-        put("n", "s");
-        put("o", "i");
-        put("ong", "j/");
-        put("ou", ".");
-        put("p", "q");
-        put("q", "f");
-        put("r", "b");
-        put("s", "n");
-        put("sh", "g");
-        put("t", "w");
-        put("u", "j");
-        put("ua", "j8");
-        put("uai", "j9");
-        put("uan", "j0");
-        put("uang", "j;");
-        put("uen", "mp");
-        put("ueng", "j/");
-        put("ui", "jo");
-        put("un", "jp");
-        put("uo", "ji");
-        put("v", "m");
-        put("van", "m0");
-        put("vang", "m;");
-        put("ve", "m,");
-        put("vn", "mp");
-        put("w", "j");
-        put("x", "v");
-        put("y", "u");
-        put("z", "y");
-        put("zh", "5");
-    }};
-    private static HashMap<String, String> XIAOHE_KEYS = new HashMap<String, String>() {{
-        put("ai", "d");
-        put("an", "j");
-        put("ang", "h");
-        put("ao", "c");
-        put("ch", "i");
-        put("ei", "w");
-        put("en", "f");
-        put("eng", "g");
-        put("ia", "x");
-        put("ian", "m");
-        put("iang", "l");
-        put("iao", "n");
-        put("ie", "p");
-        put("in", "b");
-        put("ing", "k");
-        put("iong", "s");
-        put("iu", "q");
-        put("ong", "s");
-        put("ou", "z");
-        put("sh", "u");
-        put("ua", "x");
-        put("uai", "k");
-        put("uan", "r");
-        put("uang", "l");
-        put("ui", "v");
-        put("un", "y");
-        put("uo", "o");
-        put("ve", "t");
-        put("ue", "t");
-        put("vn", "y");
-        put("zh", "v");
-    }};
-    public static Keyboard XIAOHE = new Keyboard(null, XIAOHE_KEYS, Keyboard::zero, true);
-    private static HashMap<String, String> ZIRANMA_KEYS = new HashMap<String, String>() {{
-        put("ai", "l");
-        put("an", "j");
-        put("ang", "h");
-        put("ao", "k");
-        put("ch", "i");
-        put("ei", "z");
-        put("en", "f");
-        put("eng", "g");
-        put("ia", "w");
-        put("ian", "m");
-        put("iang", "d");
-        put("iao", "c");
-        put("ie", "x");
-        put("in", "n");
-        put("ing", "y");
-        put("iong", "s");
-        put("iu", "q");
-        put("ong", "s");
-        put("ou", "b");
-        put("sh", "u");
-        put("ua", "w");
-        put("uai", "y");
-        put("uan", "r");
-        put("uang", "d");
-        put("ui", "v");
-        put("un", "p");
-        put("uo", "o");
-        put("ve", "t");
-        put("ue", "t");
-        put("vn", "p");
-        put("zh", "v");
-    }};
-    public static Keyboard ZIRANMA = new Keyboard(null, ZIRANMA_KEYS, Keyboard::zero, true);
-    private static HashMap<String, String> PHONETIC_LOCAL = new HashMap<String, String>() {{
-        put("yi", "i");
-        put("you", "iu");
-        put("yin", "in");
-        put("ye", "ie");
-        put("ying", "ing");
-        put("wu", "u");
-        put("wen", "un");
-        put("yu", "v");
-        put("yue", "ve");
-        put("yuan", "van");
-        put("yun", "vn");
-        put("ju", "jv");
-        put("jue", "jve");
-        put("juan", "jvan");
-        put("jun", "jvn");
-        put("qu", "qv");
-        put("que", "qve");
-        put("quan", "qvan");
-        put("qun", "qvn");
-        put("xu", "xv");
-        put("xue", "xve");
-        put("xuan", "xvan");
-        put("xun", "xvn");
-        put("shi", "sh");
-        put("si", "s");
-        put("chi", "ch");
-        put("ci", "c");
-        put("zhi", "zh");
-        put("zi", "z");
-        put("ri", "r");
-    }};
     public static Keyboard DAQIAN = new Keyboard(PHONETIC_LOCAL, DAQIAN_KEYS, Keyboard::standard, false);
-    public final boolean duo;
+    public static Keyboard XIAOHE = new Keyboard(null, XIAOHE_KEYS, Keyboard::zero, true);
+    public static Keyboard ZIRANMA = new Keyboard(null, ZIRANMA_KEYS, Keyboard::zero, true);
+
     final Map<String, String> local;
     final Map<String, String> keys;
     final Function<String, List<String>> cutter;
+    public final boolean duo;
 
     public Keyboard(Map<String, String> local, Map<String, String> keys,
                     Function<String, List<String>> cutter, boolean duo) {
@@ -189,6 +70,14 @@ public class Keyboard {
         this.keys = keys;
         this.cutter = cutter;
         this.duo = duo;
+    }
+
+    public String[] split(String s) {
+        return cutter.apply(local == null ? s : local.getOrDefault(s, s)).toArray(new String[]{});
+    }
+
+    public String keys(String s) {
+        return keys == null ? s : keys.getOrDefault(s, s);
     }
 
     public static List<String> standard(String s) {
@@ -213,13 +102,5 @@ public class Keyboard {
             }
             return ss;
         }
-    }
-
-    public String[] split(String s) {
-        return cutter.apply(local == null ? s : local.getOrDefault(s, s)).toArray(new String[]{});
-    }
-
-    public String keys(String s) {
-        return keys == null ? s : keys.getOrDefault(s, s);
     }
 }
